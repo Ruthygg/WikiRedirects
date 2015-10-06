671dc5246489a925ab3fddf0b79adff6b9c00faf
Ruthy_gg <ruthy_gg@hotmail.com>
2015-10-06 13:39:15 +0100

    Update WikiRedirects.R

--- R/WikiRedirects.R
+++ R/WikiRedirects.R
@@ -1,5 +1,1 @@
-#' @title Functions for Redirects
-#' @name WikiRedirects
-#' @docType package
-#'@seealso \code{\link{get_redirects}} for finding all possible redirects for a given wikipedia title
-NULL
+
