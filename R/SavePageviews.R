5e68f2e39137a41a4f3ef7085fe9b81d6f2d59b6
Ruthy_gg <ruthgarcia@oii105.oii.ox.ac.uk>
2016-08-04 17:59:06 +0100

    fixed error

--- R/SavePageviews.R
+++ R/SavePageviews.R
@@ -141,8 +141,7 @@ getAveragePageviewsfromAPI <- function (list.titles, start.date, end.date, proje
     target<- gsub(" ", "_", target)
     print(title )
     pageviews <-article_pageviews( project = project.code,  article = URLencode(target,reserved=TRUE) ,  start= start.date, end =end.date , platform=platform.code)
-    pageviews <- subset(pageviews, select=c(timestamp, views) )
-    table.result<- rbind(table.result, data.frame(title=target, avg.pageviews=mean(pageviews$views) ,sd = sd(pageviews$views), max=(pageviews$views), min=(pageviews$views)))
+    table.result<- rbind(table.result, data.frame(title=target, avg.pageviews=mean(pageviews$views) ,sd = sd(pageviews$views), max=max(pageviews$views), min=min(pageviews$views)))
   }
   
 return(table.result)  
